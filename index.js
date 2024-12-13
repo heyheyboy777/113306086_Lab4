e725c28fab4cddcfda28748c9a69503427c39f00
heyheyboy777 <wanyi48@gmail.com>
2024-12-13 15:48:12 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -31,43 +31,43 @@ document.addEventListener("keydown", function (event)
 
 	});
 	
-var drum = document.getElementById(w);
+var drum = document.getElementById("w");
 w.addEventListener("click",function(){
 	var crash = new Audio("sounds/crash.mp3");
 		crash.play();
 		animation("w");
 });
-var drum = document.getElementById(a);
+var drum = document.getElementById("a");
 a.addEventListener("click",function(){
 	var kickBass = new Audio("sounds/kick-bass.mp3");
 		kickBass.play();
 		animation("a");
 });
-var drum = document.getElementById(s);
+var drum = document.getElementById("s");
 s.addEventListener("click",function(){
 	var snare = new Audio("sounds/snare.mp3");
 		snare.play();
 		animation("s");
 });
-var drum = document.getElementById(d);
+var drum = document.getElementById("d");
 d.addEventListener("click",function(){
 	var tom1 = new Audio("sounds/tom-1.mp3");
 		tom1.play();
 		animation("d");
 });
-var drum = document.getElementById(j);
+var drum = document.getElementById("j");
 j.addEventListener("click",function(){
 	var tom2 = new Audio("sounds/tom-2.mp3");
 		tom2.play();
 		animation("j");
 });
-var drum = document.getElementById(k);
+var drum = document.getElementById("k");
 k.addEventListener("click",function(){
 	var tom3 = new Audio("sounds/tom-3.mp3");
 		tom3.play();
 		animation("k");
 });
-var drum = document.getElementById(l);
+var drum = document.getElementById("l");
 l.addEventListener("click",function(){
 	var tom4 = new Audio("sounds/tom-4.mp3");
 		tom4.play();
